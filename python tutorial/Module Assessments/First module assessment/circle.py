4e12123a32706809fec7e2c8ec49d876182367e0
hakankayir <147079586+hakankayir@users.noreply.github.com>
2024-10-13 14:57:05 -0400

    circle.py

--- python tutorial/Module Assessments/First module assessment/circle.py
+++ python tutorial/Module Assessments/First module assessment/circle.py
@@ -1,10 +1,10 @@
 while True:
   rad1=float(input("enter the radius: "))
   if rad1 <= 0:
-    print("radius cannot be 0 or a negative number", \n "please try again")
-  else:
+    print("radius cannot be 0 or a negative number please try again")
+    else:
     circ=2*3.14159*rad1
     area=3.14159*rad1*rad1
     print("circumference of the circle is ", circ, ",and the area of the circle is", area)
-  break
+    break
   
