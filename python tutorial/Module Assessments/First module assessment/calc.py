e7d1f232550122bf4f74dd383dfc97af2c57718c
hakankayir <147079586+hakankayir@users.noreply.github.com>
2024-10-13 14:14:30 -0400

    Add files via upload

---
+++ python tutorial/Module Assessments/First module assessment/calc.py
@@ -0,0 +1,10 @@
+first_val=int(input("enter the first number: "))
+second_val=int(input("enter the second number: "))
+summa=first_val+second_val
+print("sum of these numbers is", summa)
+multi=first_val*second_val
+print("multiplation of these numbers is", multi)
+divi=first_val/second_val
+print("division of the firs value by the second value is", divi)
+subst=first_val-second_val
+print("difference between the values is", subst)
