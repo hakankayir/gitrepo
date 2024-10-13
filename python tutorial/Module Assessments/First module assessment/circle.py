12f95aa2352f5e0effa2d019be32dbb44c151011
hakankayir <147079586+hakankayir@users.noreply.github.com>
2024-10-13 14:47:36 -0400

    circle.py

--- python tutorial/Module Assessments/First module assessment/circle.py
+++ python tutorial/Module Assessments/First module assessment/circle.py
@@ -1,6 +1,6 @@
 while True:
   rad1=float(input("enter the radius: "))
-  if rad1=<0:
+  if rad1 <= 0:
     print("radius cannot be 0 or a negative number", \n "please try again")
   else:
     circ=2*3.14159*rad1
